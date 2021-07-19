21ee2de9a49e84e19d4e881a393883529f5c38cc
Yalu Cai <yc468@duke.edu>
2021-07-18 22:32:30 -0400

    simple array w/o lib

--- programs/simple_array.c
+++ programs/simple_array.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
-
 int main(int argc, char *argv[]) {
     
-    int arr1[5] = {1, 2, 3, 4, 5};
+    int arr1[5] = {0, 2, 3, 4, 5};
     int arr2[5] = {0, 0, 0, 0, 0};
 
-    printf("%d\n", arr1[arr2[0]]);
+    return arr1[arr2[0]];
 
 }
