e2dd817379ead6fdc53a3f8d396f02f954fe55ea
Yalu Cai <yc468@duke.edu>
2021-07-31 16:03:43 -0400

    array access

--- programs/array_access.c
+++ programs/array_access.c
@@ -1,14 +1,8 @@
-#include <stdlib.h>
-
 struct Node
 {
     int field[10];
     struct Node* next;
 };
-
-void do_work(struct Node* node) {
-    node->field[6] = node->field[5] + node->field[4];
-}
 
 void simple_chase(struct Node *node, int count){
     for (int i = 0; i < count; i++)
@@ -19,18 +13,6 @@ void simple_chase(struct Node *node, int count){
     
 }
 
-int main(int argc, char *argv[]) {
-    
-    struct Node* node;
-    node->next = NULL;
-    
-    for (int i = 0; i < 10; i++)
-    {
-        node->field[i] = 0;
-    }
-    
-    simple_chase(node, 0);
-
-    return 0;
-
+void do_work(struct Node* node) {
+    node->field[6] = node->field[5] + node->field[4];
 }
