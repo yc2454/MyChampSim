34ff343d073ff615dfcb8515b73225e50d239d23
Yalu Cai <yc468@duke.edu>
2021-08-16 17:13:28 -0400

    shell code to generate new trace

---
+++ generate_new_trace.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../pin-3.2-81205-gcc-linux/pin -injection child -ifeellucky -t MyChampSim/tracer/obj-intel64/champsim_tracer.so -o simple_array_trace_my -- MyChampSim/programs/simple_array
